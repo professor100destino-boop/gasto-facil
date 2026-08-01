c385b0283ad2a1e14eb987aea8256ff1477bfdb0
Professor 100 Destino <professor100destino@gmail.com>
2026-08-01 08:52:01 -0300

    Adiciona funcionamento offline

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='gasto-facil-v1';
+const FILES=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return res}).catch(()=>caches.match('./index.html'))));
+});
